45e72532daf14983af7ac25d441d172ec9de2bf2
nwmei <nwmei@bu.edu>
2017-12-23 15:31:33 -0500

    included labels that show price change since last update

--- webscrape_cryptocurrency.py
+++ webscrape_cryptocurrency.py
@@ -25,6 +25,11 @@ def write_prices(data_list):
     time.set('last updated: '+str(datetime.datetime.now().time())[:-7])
     for i in range(len(data_list)):
         entryTextPointers[i].set(data_list[i])
+        #display price change
+        if len(prevPriceList)!=0:
+            diff = float(data_list[i])-float(prevPriceList[i])
+            changeLabelsList[i].set(round(diff, 3))
+    
 
 def start_scraping():
     """scrapes the web urls for the price of each cryptocurrency
@@ -49,13 +54,7 @@ def start_scraping():
     for pageSoup in pageSoupList:
         priceList.append(str(pageSoup.findAll("span",{"id":"quote_price"})).split()[6][23:-7])
 
-    write_prices(priceList)
-    
-    #determine whether or not a price has changed
-    
-    if priceList != prevPriceList:
-        print(priceList, prevPriceList)
-        print('~One of the prices just changed!')
+    write_prices(priceList) #call function to display new text
     prevPriceList = priceList[:]
     
 scrape_again = 1 #initialize to some value
@@ -93,7 +92,7 @@ for currency in currencyList:
 
     Label(frame, text='{}'.format(currency)).grid(row=0,column=columnCount)
     currencyEntry = Entry(frame, textvariable=entryText)
-    currencyEntry.grid(row=1, column=columnCount, padx = 5, pady=5)
+    currencyEntry.grid(row=1, column=columnCount, padx = 5, pady=0)
     currencyEntryList.append(currencyEntry)
 
     columnCount+=1
@@ -101,21 +100,32 @@ for currency in currencyList:
 #start scraping button
 startButton = Button(frame, text='start scraping')
 startButton.bind('<Button-1>', base)
-startButton.grid(row = 2, column=0, sticky=W, padx=10, pady=4)
+startButton.grid(row = 3, column=0, sticky=W, padx=10, pady=4)
 
 #stop scraping button
 endButton = Button(frame, text='stop scraping')
 endButton.bind('<Button-1>', stop_infinite)
-endButton.grid(row = 3, column=0, sticky=W, padx=10)
+endButton.grid(row = 4, column=0, sticky=W, padx=10)
+
+#show change since last 20 seconds
+Label(frame, text='20(s) change:').grid(row=2, column=0, sticky=W, padx=5)
+changeLabelsList = []
+columnCount2=0
+for currency in currencyList:
+    priceChange = StringVar()
+    changeLabelsList.append(priceChange)
+    Label(frame, textvariable=priceChange).grid(row=2, column=columnCount2, sticky=E, padx=3)
+    priceChange.set('0.0')
+    columnCount2 += 1
 
 #show number of iterations
 iterationLabel = Label(frame, textvariable=iterations)
-iterationLabel.grid(row=4, column=0, sticky=W, padx=5)
+iterationLabel.grid(row=5, column=0, sticky=W, padx=5)
 iterations.set('iterations: 0')
 
 #show last updated
 updatedLabel = Label(frame, textvariable=time)
-updatedLabel.grid(row=4, column=1, sticky=W)
+updatedLabel.grid(row=5, column=1, sticky=W)
 time.set('last updated:')
 
 frame.pack()
